90e42bf26566884e9b9416b82c9217396fc68a58
Mateusz Fryc <mateuszfryc@gmail.com>
2024-12-03 13:46:14 +0100

    fix: missing port bind

--- index.js
+++ index.js
@@ -132,7 +132,9 @@ app.post('/logout', authenticateToken, (req, res) => {
   res.status(200).json({ message: 'Logged out successfully' });
 });
 
+const PORT = process.env.PORT || 3000;
+
 // Start the server
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
