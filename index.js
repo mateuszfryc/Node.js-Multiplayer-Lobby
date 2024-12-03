c753debfe1a8ab0d6d8280da4de2a4e2d39ab14e
Mateusz Fryc <mateuszfryc@gmail.com>
2024-12-03 14:03:36 +0100

    fix: ssl connection

--- index.js
+++ index.js
@@ -58,6 +58,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const authenticateToken = (req, res, next) => {
